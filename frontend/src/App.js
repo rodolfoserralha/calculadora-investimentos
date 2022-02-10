9b2621560c65d3d315efbc0c49eb8c82dbc56a5f
Rodolfo <rodolfoserralha@hotmail.com>
2022-02-10 10:37:44 -0300

    Feat: add css to formulary, icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import SimulatorLeft from './components/LeftFormulary';
+import SimulatorRight from './components/RightFormulary';
 
 function App() {
   return (
     <div id="root">
       <h1 className="title">Simulador de Investimentos</h1>
+      <div id="left-forms-container">       
+        <div id="left-forms">
+          <SimulatorLeft />
+          <SimulatorRight />
+        </div>
+      </div>
     </div>
   );
 }
